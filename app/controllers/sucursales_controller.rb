9c7660f10a59bcb585b8b116cf6914606e7c9534
Juan Carlos Santiago <krlos013@gmail.com>
2015-12-14 22:37:11 -0600

    app3

--- app/controllers/sucursales_controller.rb
+++ app/controllers/sucursales_controller.rb
@@ -1,4 +1,36 @@
 class SucursalesController < ApplicationController
   def index
+  	  #tiene acceso a todo el grupo?
+ if params[:type] == "Servicio"
+     @grupo = Acceso.where(:user_id => current_user.id).pluck(:group_id).first
+     if @grupo.blank?
+            @agencia = Acceso.where(:user_id => current_user.id).pluck(:app_id_s).first
+          if @agencia.blank?
+              
+            else
+            @apps = App.where(:api_key => @agencia).where(:type_app => params[:type])
+         
+         end
+     else
+        @apps = App.where(:group => @grupo).where(:type_app => params[:type])
+       
+     end
+end
+if params[:type] == "HYP"
+@grupo = Acceso.where(:user_id => current_user.id).pluck(:group_id).first
+     if @grupo.blank?
+        @agencia_h = Acceso.where(:user_id => current_user.id).pluck(:app_id_h).first
+                  if @agencia_h.blank?
+                    
+                    else
+                    @apps = App.where(:api_key => @agencia_h).where(:type_app => params[:type])
+                 end
+
+    else
+        @apps = App.where(:group => @grupo).where(:type_app => params[:type])
+        
+    end
+  end
+
   end
 end
