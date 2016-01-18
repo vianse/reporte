df9e3466f3996c194c5d7f90aad6b81d2962a36d
Juan Carlos Santiago <krlos013@gmail.com>
2016-01-18 13:27:50 -0600

    app4

--- app/controllers/api/v1/get_fecha_controller.rb
+++ app/controllers/api/v1/get_fecha_controller.rb
@@ -11,19 +11,19 @@ class Api::V1::GetFechaController <ApplicationController
  end
  private
  def mes(value)
-    if value == "01"
-        mes ="Enero"
-    elsif value == "02" then mes = "Febrero del "
-    elsif value == "03" then mes = "Marzo del "
-    elsif value == "04" then mes = "Abril del "
-    elsif value == "05" then mes = "Mayo del "
-    elsif value == "06" then mes = "Junio del "
-    elsif value == "07" then mes = "Julio del "
-    elsif value == "08" then mes = "Agosto del "
-    elsif value == "09" then mes = "Septiembre del "
-    elsif value == "10" then mes = "Octubre del "
-    elsif value == "11" then mes = "Noviembre del "
-    elsif value == "12" then mes = "Diciembre del "
+    if value == "01" || value == "1"
+        mes ="Enero del "
+    elsif value == "02" || value == "2" then mes = "Febrero del "
+    elsif value == "03" || value == "3" then mes = "Marzo del "
+    elsif value == "04" || value == "4" then mes = "Abril del "
+    elsif value == "05" || value == "5" then mes = "Mayo del "
+    elsif value == "06" || value == "6" then mes = "Junio del "
+    elsif value == "07" || value == "7" then mes = "Julio del "
+    elsif value == "08" || value == "8" then mes = "Agosto del "
+    elsif value == "09" || value == "8" then mes = "Septiembre del "
+    elsif value == "10"  then mes = "Octubre del "
+    elsif value == "11"  then mes = "Noviembre del "
+    elsif value == "12"  then mes = "Diciembre del "
     end
     return mes
     end
--- app/controllers/api/v1/get_periodos_anteriores_controller.rb
+++ app/controllers/api/v1/get_periodos_anteriores_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::GetPeriodosAnterioresController <ApplicationController
 	def index
    		@año = Date.today.year
 		@parametro = params[:app_id]		
-		meses = Facturada.select(:mes).uniq.order('mes asc')
+		meses = Facturada.select(:mes).where(:anio => @año).uniq.order('mes asc')
 		tipo = params[:type]
 
 		elementos = meses.map do |k|
@@ -37,16 +37,16 @@ class Api::V1::GetPeriodosAnterioresController <ApplicationController
 private
 
  def mes(value)
-    if value == "01"
+    if value == "01" || value == "1"
         mes ="Enero"
-    elsif value == "02" then mes = "Febrero"
-    elsif value == "03" then mes = "Marzo"
-    elsif value == "04" then mes = "Abril"
-    elsif value == "05" then mes = "Mayo"
-    elsif value == "06" then mes = "Junio"
-    elsif value == "07" then mes = "Julio"
-    elsif value == "08" then mes = "Agosto"
-    elsif value == "09" then mes = "Septiembre"
+    elsif value == "02" || value == "2" then mes = "Febrero"
+    elsif value == "03" || value == "3" then mes = "Marzo"
+    elsif value == "04" || value == "4" then mes = "Abril"
+    elsif value == "05" || value == "5" then mes = "Mayo"
+    elsif value == "06" || value == "6" then mes = "Junio"
+    elsif value == "07" || value == "7" then mes = "Julio"
+    elsif value == "08" || value == "8" then mes = "Agosto"
+    elsif value == "09" || value == "9" then mes = "Septiembre"
     elsif value == "10" then mes = "Octubre"
     elsif value == "11" then mes = "Noviembre"
     elsif value == "12" then mes = "Diciembre"
