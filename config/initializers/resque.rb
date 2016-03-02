72f12c4e7445073ddcfec3d5ec6a40a6816d173b
Juan Carlos Santiago <krlos013@gmail.com>
2016-03-02 13:55:11 -0600

    redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,5 @@
 #Resque.redis = '162.243.48.92:6379'
-redis_client = Redis.new(:url => "redis://root:hola123,@162.243.48.92:6379")
+#redis_client = Redis.new(:url => "redis://root:hola123,@162.243.48.92:6379")
+redis_client = Redis.new(url: (ENV['REDISTOGO_URL'] || 'redis://root:hola123,@162.243.48.92:6379'))
+#Redis.new(url: (ENV['REDISTOGO_URL'] || 'redis://root:hola123,@162.243.48.92:6379'))
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
